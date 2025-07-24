8559f3ba90059fc780dea233fa24945f733e4667
Asher9889 <saursbhkushwaha9889@gmail.com>
2025-07-24 12:40:34 +0530

    Login page orientation fixed

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,7 +2,7 @@ import { UserForm } from "../../components"
 
 const Login = () => {
     return (
-        <div className="min-h-[calc(100vh-100px)]">
+        <div className="min-h-[calc(100vh-100px)] mb-10">
             <div className="mx-auto max-w-[600px]">
                 <UserForm />
             </div>
