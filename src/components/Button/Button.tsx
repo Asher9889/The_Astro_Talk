08329e81acf0a638801300bd9606dd9af9aaac38
Asher9889 <saursbhkushwaha9889@gmail.com>
2025-07-24 12:34:56 +0530

    Fix: No button navigation

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,14 @@
 import { useNavigate } from "react-router-dom";
 
-const Button = ({className, content, navigate}:{className?: string, content:string, navigate?: string})=>{
+const Button = ({className, content, navigate}:{className?: string, content:string, navigate?: string | undefined})=>{
     const navigatee = useNavigate();
+
+    function navigation (){
+        if(!navigate) return;
+        navigatee(`/${navigate}`)
+    }
     return (
-        <button onClick={()=> navigatee(`/${navigate}`)} className={`px-6 py-2 text-sm font-bold rounded-md cursor-pointer transition-all duration-300 ${className || ""}`}>
+        <button onClick={navigation} className={`px-6 py-2 text-sm font-bold rounded-md cursor-pointer transition-all duration-300 ${className || ""}`}>
             {content}
         </button>
     )
--- src/pages/Home/CTA-Page/CTA.tsx
+++ src/pages/Home/CTA-Page/CTA.tsx
@@ -1,14 +1,14 @@
 import { Button } from "../../../components";
 
 
-const CTA = ()=>{
-  
+const CTA = () => {
+
     return (
         <div className="py-8  text-center max-w-[90%] flex flex-col justify-center items-center gap-20 text-(--color-button-text)">
             <h1 className="text-3xl font-bold text-(--color-text-primary)">Would you like to understand yourself better using Vedic Astro?</h1>
-            <div  className="flex flex-row justify-center items-center gap-20">
-                <Button navigate="register" className="bg-(--color-text-secondary) hover:bg-(--color-text-primary)  px-8 font-bold" content="Yes"/>
-                <Button className="bg-(--color-link)  hover:bg-(--color-button-secondary) px-8" content="No"/>
+            <div className="flex flex-row justify-center items-center gap-20">
+                <Button navigate="register" className="bg-(--color-text-secondary) hover:bg-(--color-text-primary)  px-8 font-bold" content="Yes" />
+                <Button className="bg-(--color-link)  hover:bg-(--color-button-secondary) px-8" content="No" />
             </div>
         </div>
     )
