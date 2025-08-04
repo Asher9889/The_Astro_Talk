95dbd64f3d021da87a49aaf74f88162510b839ae
Asher9889 <saurabhkushwaha9889@gmail.com>
2025-08-04 23:29:01 +0530

    talk to astro added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,12 +12,17 @@ const Header = ()=>{
                 <button className="w-22 cursor-pointer">
                     <img onClick={()=> navigate("/")} src={tla} alt="TLA Logo" />
                 </button>
+
+                <ul className="flex flex-row gap-6 justify-center items-center gap-16">
+                    <li><button className="text-md cursor-pointer text-(--color-text-primary) hover:scale-[0.98] transition-all">Talk to an Astrologer</button></li>
+                    <li><button className="cursor-pointer text-(--color-text-primary) hover:scale-[0.98] transition-all">Astrology Insight</button></li>
+                </ul>
                 {/* <div className="flex flex-row gap-4 md:gap-6 lg:gap-8 item-center justify-center  text-(--color-text-white)">
                     {navMenu.map((menu) => (
                         <NavLink className={`hover:text-(--color-text-hover) hover:scale-99 transition-all duration-300`} to={`/${menu.toLowerCase().split(" ").join("_")}`}>{menu}</NavLink>
                     ))}
                 </div> */}
-                <div className="flex flex-row justify-center items-center gap-4">
+                <div className="flex flex-row justify-center items-center">
                     {/* <select name="lnguage" id="" className="bg-zinc-300 p-2 rounded-md cursor-pointer">
                         <option value="hi">Hindi</option>
                         <option value="en">English</option>
